761c5953f6a34f0d5f033980d0f65dd4b7cbb4fa
Jakob Penninger <jakob.penninger@gmx.net>
2020-08-27 10:21:45 +0200

    Add EinfacherRechenAufgabe

---
+++ src/EinfacherRechenAufgabe.java
@@ -0,0 +1,21 @@
+public class EinfacherRechenAufgabe {
+
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 1;
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+
+        float num3 = 1.0f;
+        float num4 = 3.0f;
+        System.out.println(num3 + " + " + num4 + " = " + (num3 + num4));
+        System.out.println(num3 + " - " + num4 + " = " + (num3 - num4));
+        System.out.println(num3 + " * " + num4 + " = " + (num3 * num4));
+        System.out.println(num3 + " / " + num4 + " = " + (num3 / num4));
+    }
+
+
+}
