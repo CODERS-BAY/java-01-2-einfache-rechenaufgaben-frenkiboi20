8fff1efae0d30c5f9b7a396e9c4bbadd82104bf9
Jakob Penninger <jakob.penninger@gmx.net>
2020-08-27 11:49:09 +0200

    Change NumberFormatting

--- src/EinfacherRechenAufgabe.java
+++ src/EinfacherRechenAufgabe.java
@@ -4,17 +4,20 @@ public class EinfacherRechenAufgabe {
 
         int num1 = 3;
         int num2 = 1;
-        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
-        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
-        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
-        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
-
+        System.out.format("%d", num1);
+        System.out.print(" + ");
+        System.out.format("%d", num2);
+        System.out.print(" = ");
+        System.out.format("%d", num1+num2);
+        System.out.println();
         float num3 = 1.0f;
         float num4 = 3.0f;
-        System.out.println(num3 + " + " + num4 + " = " + (num3 + num4));
-        System.out.println(num3 + " - " + num4 + " = " + (num3 - num4));
-        System.out.println(num3 + " * " + num4 + " = " + (num3 * num4));
-        System.out.println(num3 + " / " + num4 + " = " + (num3 / num4));
+        System.out.format("%.1f", num3);
+        System.out.print(" + ");
+        System.out.format("%.1f", num4);
+        System.out.print(" = ");
+        System.out.format("%.1f", (num3+num4));
+
     }
 
 
